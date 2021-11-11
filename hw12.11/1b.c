acc1005cdf50223658ecbf08b0802dc9c2afb765
n0men-nesci0 <timur.pak.bsn@gmail.com>
2021-11-11 10:43:40 +0300

    hw 12.11 task 1.b

--- hw12.11/1b.c
+++ hw12.11/1b.c
@@ -5,31 +5,34 @@
 #include <wait.h>
 #include <errno.h>
 
-//pr1 arg1 arg2 | pr2; pr3 >> f1.dat
+//pr1 < f1.dat | pr2 > f2.dat; pr3
 
 int main(int argc, char** argv) {
     int fd[2];
     pipe(fd);
     if (!fork()) { // child pr1
+      int fd_in = open(argv[2], O_RDONLY);
+      dup2(fd_in, 0);
       dup2(fd[1], 1);
+      close(fd_in);
       close(fd[1]);
       close(fd[0]);
-      execlp(argv[1], argv[1], argv[2], argv[3], NULL); // pr1(arg1,arg2)->fd[1]
+      execlp(argv[1], argv[1], NULL); // pr1(arg1,arg2)->fd[1]
     }
     else // parent
       if (!fork()) { // child pr2
+        int fd_out = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+        dup2(fd_out, 1);
         dup2(fd[0], 0);
+        close(fd_out);
         close(fd[0]);
         close(fd[1]);
-        execlp(argv[4], argv[4], NULL); // pr2<-fd[0]
+        execlp(argv[3], argv[3], NULL); // pr2<-fd[0]
       }
     // parent
     close(fd[0]);
     close(fd[1]);
-    int fd1 = open(argv[6], O_WRONLY | O_APPEND | O_CREAT, 0644);
     wait(NULL);
     wait(NULL);
-    dup2(fd1, 1);
-    close(fd1);
     execlp(argv[5], argv[5], NULL);
 }
