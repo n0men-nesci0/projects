20e2746af3dd375356e1871a83532442f21142ff
Timur <timur.pak.bsn@gmail.com>
2022-05-15 13:43:35 +0300

    qq

--- do_switch.py
+++ do_switch.py
@@ -2,9 +2,9 @@ import sys
 f = open("input.txt")
 sys.stdout = open("output.txt", 'w')
 for line in f:
-    lst = [word[:-1] for word in line.split()]
+    lst = [word.replace(',', '') for word in line.split()]
     for word in lst:
         print("case " + word + ":")
-        print("\tstr_lex = " + '"' + word[4:] + '"' + ";")
+        print("\tstr_lex = " + '"' + word.replace("LEX_", '') + '"' + ";")
         print("\tbreak;")
     
